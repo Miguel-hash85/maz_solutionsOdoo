7faede39de4922248359e08dfd7f8e2943646674
zyaqoob <z332han@gmail.com>
2022-02-02 12:29:25 +0100

    ExamSession exception

--- shared/maz_solutions/models/ExamSession.py
+++ shared/maz_solutions/models/ExamSession.py
@@ -1,7 +1,8 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import  ValidationError
 
 class ExamSession (models.Model):
     
@@ -16,3 +17,17 @@ class ExamSession (models.Model):
     exam=fields.Many2one('maz_solutions.exam', string="Exam")
     
     mark=fields.Integer()
+
+    @api.constrains('dateTimeEnd','dateTimeStart')
+    def _check_dates(self):
+        for record in self:
+            if record.dateTimeStart > record.dateTimeEnd:
+                raise ValidationError("Sorry, End date must be greater that Start date")
+            
+    @api.constrains('mark')
+    def _check_mark(self):
+        for record in self:
+            if record.mark > 10:
+                raise ValidationError("Sorry, mark should not be greater than 10")
+            if record.mark < 0:
+                raise ValidationError("Sorry, mark should not be lower than 0")
